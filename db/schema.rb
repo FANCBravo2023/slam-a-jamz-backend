d3729ff7e0f7cf124b3390578b3fb337d9a30041
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-05-16 16:17:00 -0700

    completed rails jwt with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_214512) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_225830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_214512) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "date"
+  end
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
