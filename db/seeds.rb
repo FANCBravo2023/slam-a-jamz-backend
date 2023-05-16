159badab5e799e6939edc734808c06d8086b6092
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-05-16 11:47:36 -0700

    completed backend-config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
+
+events = [
+  {
+    user_id: 1,
+    date: "May 21, 2023",
+    time: "8pm",
+    venue: "Pages Arena",
+    street: "123 Croissant St",
+    city: "Bakersfield",
+    state: "CA",
+    price: 94,
+  },
+  {
+    user_id: 2,
+    date: "May 28, 2023",
+    time: "11pm",
+    venue: "Butcher Chop",
+    street: "456 Baggette St",
+    city: "Bismo Bich",
+    state: "CA",
+    price: 35,
+  },
+  {
+    user_id: 3,
+    date: "May 30, 2023",
+    time: "10pm",
+    venue: "Skyway",
+    street: "789 Wonder Bread Ln",
+    city: "Maple Grove",
+    state: "MN",
+    price: 100,
+  }
+]
+
+events.each do |event|
+  Event.create event
+  puts "creating event #{event}"
+end
+
+
