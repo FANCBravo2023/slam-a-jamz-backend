09efffcedcabdbdfc5fc75fa18cf4c4df654cc7f
Nguyen3464 <n.nguyen3464@gmail.com>
2023-05-30 15:54:59 -0700

    finish deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,11 +4,9 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # bundle exec rails db:reset to reset backend db
-bundle exec rails db:reset
+# bundle exec rails db:reset
 
-# bundle exec rake db:migrate
+bundle exec rake db:migrate
 # after initial deployment remove db:seed line out
 # bundle exec rake db:seed
 
-# fix seeds, remove db:reset, uncomment out db:migrate, remove rake db:seed
-# remove enviorment variable on render
